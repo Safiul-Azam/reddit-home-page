f4c8b935f7a2ecc8aa914f9b8a5024a8e811a895
Safiul-Azam <safiulazamriad@gmail.com>
2022-09-22 09:39:44 +0600

    new branch for stepper

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h2 className="text-4xl text-green-500">start simple E-commerce site with steps form </h2>
+      <h2 className="text-4xl text-green-500">start simple E-commerce site with steps form</h2>
     </div>
   );
 }
---
+++ src/component/Stepper.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Stepper = () => {
+    return (
+        <div>
+            <h3>stepper form component</h3>
+        </div>
+    );
+};
+
+export default Stepper;
