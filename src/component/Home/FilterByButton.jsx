600a9e37070c8ec7e78e97cbef0ea71c9b16afb1
Safiul-Azam <safiulazamriad@gmail.com>
2022-09-23 17:05:45 +0600

    filter low and high price and active btn style

--- src/component/Home/FilterByButton.jsx
+++ src/component/Home/FilterByButton.jsx
@@ -1,33 +1,39 @@
 import React, { useState } from 'react';
 import useProducts from '../../hooks/useProducts';
 import ShowProducts from './ShowProducts';
-import { GiRunningShoe, GiClothes,GiOfficeChair} from 'react-icons/gi';
+import { GiRunningShoe, GiClothes, GiOfficeChair } from 'react-icons/gi';
 import { TbDeviceWatchStats } from 'react-icons/tb';
-import { MdOtherHouses } from 'react-icons/md';
+import { MdOutlineMore } from 'react-icons/md';
+import { FaHome } from 'react-icons/fa';
 
 const FilterByButton = () => {
     const [products] = useProducts()
     const [filterByBtn, setFilterByBtn] = useState([])
-    const [productName, setProductName] =useState('')
+    const [productName, setProductName] = useState('')
+    const [active,setActive] =useState('all')
+
+    
     const btnByProductName = [
+        { id: 2, name: 'all', icon: <FaHome /> },
         { id: 2, name: 'clothes', icon: <GiClothes /> },
         { id: 3, name: 'shoes', icon: <GiRunningShoe /> },
         { id: 4, name: 'electronics', icon: <TbDeviceWatchStats /> },
         { id: 5, name: 'furniture', icon: <GiOfficeChair /> },
-        { id: 1, name: 'others', icon: <MdOtherHouses /> },
+        { id: 1, name: 'others', icon: <MdOutlineMore className=' rotate-180' /> },
     ]
 
     const handleClick = (btn) => {
         setFilterByBtn(products.filter(product => product?.category?.name.toLowerCase() === btn))
         setProductName(products.find(product => product?.category?.name.toLowerCase() === btn))
+        setActive(btn)
     }
     return (
         <>
             <div className='flex justify-center space-x-10 mb-20'>
                 {
-                    btnByProductName.map((btn) => <div className='border flex justify-center w-28 h-28' key={btn.id}>
+                    btnByProductName.map((btn) => <div className={`border flex justify-center w-28 h-28 hover:bg-teal-500 hover:text-white ${active === btn.name ? 'bg-teal-500 text-white':''}`} key={btn.id}>
                         <button className='' onClick={() => handleClick(btn.name)}>
-                            <h3 className='text-7xl'>{btn.icon}</h3>
+                            <h3 className='text-5xl'>{btn.icon}</h3>
                             <p className='text-lg capitalize'>{btn.name}</p>
                         </button>
                     </div>)
--- src/component/Home/ShowProducts.jsx
+++ src/component/Home/ShowProducts.jsx
@@ -1,43 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useProducts from '../../hooks/useProducts';
 import { AiOutlineShoppingCart } from 'react-icons/ai'
 import { BiAddToQueue } from 'react-icons/bi'
 
-const ShowProducts = ({ filterByBtn, productName }) => {
+const ShowProducts = ({ filterByBtn, productName, handleChange }) => {
     const [products] = useProducts()
-    console.log(products);
+    const [option, setOption] = useState([])
+    const [option2, setOption2] = useState([])
+
+    const handleLow = () => {
+        setOption(filterByBtn.sort((a, b) => a.price - b.price))
+        option2 && setOption([])
+    }
+    const handleHigh = () => {
+        setOption2(filterByBtn.sort((a, b) => b.price - a.price))
+        option && setOption2([])
+    }
     return (
         <div className='container  mx-auto '>
-            {filterByBtn.length > 0 && <div className='border rounded-md shadow-md mb-4 p-3 flex justify-between'>
+            {<div className='border rounded-md shadow-md mb-4 p-3 flex justify-between'>
                 <div>
                     <h2 className='text-xl font-semibold'>All {productName?.category?.name}</h2>
                 </div>
                 <div className='space-x-8 flex '>
                     <div>
                         <span className='text-sm text-gray-600 font-medium'>Show: </span>
-                        <select className='bg-gray-100 focus:outline-none text-sm px-3 py-2'>
-                            <option value="" selected>5</option>
-                            <option value="">10</option>
-                            <option value="">15</option>
-                            <option value="">20</option>
+                        <select defaultValue={products} onChange={handleChange} className='bg-gray-100 focus:outline-none text-sm px-3 py-2'>
+                            <option value="5" selected>5</option>
+                            <option value="10">10</option>
+                            <option value="15">15</option>
+                            <option value="20">20</option>
                         </select>
                     </div>
-                    <div>
-                        <span className='text-sm text-gray-600 font-medium'>Sort By: </span>
-                        <select className='bg-gray-100 focus:outline-none text-sm px-3 py-2'>
-                            <option value="" selected>Default</option>
-                            <option value="">Low to Hight</option>
-                            <option value="">Hight To Low</option>
-                        </select>
+                    <div className=' space-x-5 flex'>
+                        <button className='hover:bg-teal-700 bg-teal-100 hover:text-white text-teal-500 text-sm p-1 flex items-center justify-center hover:duration-500 hover:ease-in-out ease-in-out duration-500' onClick={handleLow}>Low To High</button>
+                        <button className='hover:bg-teal-700 bg-teal-100 hover:text-white text-teal-500 text-sm p-1 flex items-center justify-center hover:duration-500 hover:ease-in-out ease-in-out duration-500' onClick={handleHigh}>High To Low</button>
                     </div>
                 </div>
             </div>}
             <div className='grid grid-cols-5 gap-5'>
                 {filterByBtn?.length > 0 ?
-                    filterByBtn.map(product => <div key={product.id} className='border h-80'>
+                    filterByBtn.map(product => <div key={product.id} className='border pb-3'>
                         <img src={product?.images[1] || product.category.image} alt="" />
                         <div className='px-3 mt-2'>
-                            <h2 className='text-lg text-center font-semibold'>{product.title}</h2>
+                            <h2 className='text-lg font-semibold'>{product.title}</h2>
+                            <p className='text-sm font-medium'>category :- {product.category.name}</p>
+                            <hr className='my-2' />
+                            <h3 className='text-xl text-center text-amber-600'>${product.price}</h3>
+                        </div>
+                        <div className='px-2 space-y-2 flex flex-col my-2'>
+                            <button className='hover:bg-teal-700 bg-teal-100 hover:text-white text-teal-500 text-sm p-1 flex items-center justify-center hover:duration-500 hover:ease-in-out ease-in-out duration-500'>
+                                <AiOutlineShoppingCart className='text-xl mr-3' />
+                                Buy Now
+                            </button>
+                            <button className='hover:bg-gray-200 bg-white text-sm p-1 flex items-center justify-center hover:duration-200 hover:ease-in-out ease-in-out duration-500'>
+                                <BiAddToQueue className='text-xl mr-3' />
+                                Add To Cart
+                            </button>
                         </div>
                     </div>) :
                     products.map(product => <div key={product.id} className='border pb-3'>
